c7e6533df586bbe09aab82b462409a1d1c0572f2
EzeBernardine <demayous2@gmail.com>
2018-06-12 14:59:01 +0100

    indicate error while typing

--- GAME/game.js
+++ GAME/game.js
@@ -39,17 +39,41 @@ function correct(e) {
     console.log(type);
     if (type === theOther) {
         document.getElementById("type").style.color = "green";
-        document.getElementById("error").innerHTML=("Weldone");
+        document.getElementById("error").innerHTML = ("Weldone");
         document.getElementById("error").style.color = "green";
     } else {
         document.getElementById("type").style.color = "red";
-        document.getElementById("error").innerHTML=("error!!! Pls try again");
+        document.getElementById("error").innerHTML = ("error!!! Pls try again");
         document.getElementById("error").style.color = "red";
     }
 }
- 
-
-
-
 
 let red = document.getElementById("red").addEventListener("click", correct)
+//let lenght=document.getElementById("type").value.lenght;
+//alert(length);
+
+function display(e) {
+    let len = document.getElementById("type").value.length;
+
+
+    if (e.charCode == 32) {
+        //alert(len);
+        for (let i = 0; i < len; i++) {
+
+            let index0 = document.getElementById("type").value.charAt(i);
+            // console.log(index0);
+            let theOther = (diffWords[presentWord]).charAt(i);
+
+            console.log(theOther);
+            if (index0 == theOther) {
+                //console.log("i did");
+                document.getElementById("type").style.color = "green";
+            } else {
+                //console.log("itsnot");
+                document.getElementById("type").style.color = "red";
+            }
+            e.preventDefault
+        }
+    }
+}
+let typed = document.getElementById("type").addEventListener("keypress", display)

bba11f7779bde4fb76b4f889f6e222f7ef189cbd
EzeBernardine <demayous2@gmail.com>
2018-06-13 16:17:54 +0100

    non working mode

--- GAME/game.js
+++ GAME/game.js
@@ -29,49 +29,50 @@ function shuffle(e) {
     let theDiv = document.getElementById("words").innerHTML = (theWord);
     e.preventDefault();
 }
-let green = document.getElementById("green").addEventListener("click", shuffle)
+let green =document.getElementById("green").addEventListener("click", shuffle)
+
+
+let typedWord= document.getElementById("type");
+let errorMessage=document.getElementById("error")
+
 
 function correct(e) {
     let theOther = diffWords[presentWord]
-    let type = document.getElementById("type").value;
-    // alert(type);
-    // alert(diffWords[presentWord]);
-    console.log(type);
+    let type = typedWord.value;
     if (type === theOther) {
-        document.getElementById("type").style.color = "green";
-        document.getElementById("error").innerHTML = ("Weldone");
-        document.getElementById("error").style.color = "green";
+        typedWord.style.color = "green";
+        errorMessage.innerHTML = ("Weldone");
+        errorMessage.style.color = "green";
     } else {
-        document.getElementById("type").style.color = "red";
-        document.getElementById("error").innerHTML = ("error!!! Pls try again");
-        document.getElementById("error").style.color = "red";
+        typedWord.style.color = "red";
+        errorMessage.innerHTML = ("error!!! Pls try again");
+        errorMessage.style.color = "red";
     }
 }
 let red = document.getElementById("red").addEventListener("click", correct)
-//let lenght=document.getElementById("type").value.lenght;
-//alert(length);
+
 
 
 
 function display(e) {
-    let len = document.getElementById("type").value.length;
-    if (e.charCode == 32) {
+    let len =typedWord.value.length;
+    if (e.charCode === 32) {
         //alert(len);
         for (let i = 0; i < len; i++) {
-            let index0 = document.getElementById("type").value.charAt(i);
+            let index0 = typedWord.value.charAt(i);
             // console.log(index0);
             let theOther = (diffWords[presentWord]).charAt(i);
             console.log(theOther);
             if (index0 == theOther) {
                 //console.log("i did");
-                document.getElementById("type").style.color = "green";
+                typedWord.style.color = "green";
             } else {
                 //console.log("itsnot");
-                document.getElementById("type").style.color = "red";
+                typedWord.style.color = "red";
             }
-            e.preventDefault
+            e.preventDefault();
         }
     }
 }
 
-let typed = document.getElementById("type").addEventListener("keypress", display)
+let typed = typedWord.addEventListener("keypress", display)
